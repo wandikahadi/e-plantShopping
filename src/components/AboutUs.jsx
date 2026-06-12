f47e2822808266c2cc66525d7402057051492f6a
wandika26 <12k_wandika@apps.ipb.ac.id>
2026-06-12 14:03:39 +0700

    Add AboutUs

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,33 @@
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <div className="text-left text-lg font-semibold leading-relaxed md:text-xl">
+      <p className="mb-6 text-center">
+        Welcome to Paradise Nursery, where green meets serenity!
+      </p>
+
+      <p className="mb-6">
+        At Paradise Nursery, we are passionate about bringing nature closer
+        to you. Our mission is to provide a wide range of high-quality
+        plants that not only enhance the beauty of your surroundings but
+        also contribute to a healthier and more sustainable lifestyle. From
+        air-purifying plants to aromatic fragrant ones, we have something
+        for every plant enthusiast.
+      </p>
+
+      <p className="mb-6">
+        Our team of experts is dedicated to ensuring that each plant meets
+        our strict standards of quality and care. Whether you're a seasoned
+        gardener or just starting your green journey, we're here to support
+        you every step of the way. Feel free to explore our collection, ask
+        questions, and let us help you find the perfect plant for your home
+        or office.
+      </p>
+
+      <p>
+        Join us in our mission to create a greener, healthier world. Visit
+        Paradise Nursery today and experience the beauty of nature right at
+        your doorstep.
+      </p>
+    </div>
   )
 }
